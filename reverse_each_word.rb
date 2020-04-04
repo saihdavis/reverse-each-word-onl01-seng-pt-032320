d5e8ea4f7da40dc304081062bb25fc721a9f0c15
Saida Davis <saihdavis@gmail.com>
2020-04-04 16:53:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
-def reverse_each_word(string)
-  string.collect do |strings|
- #{string}
+def reverse_each_word(sentence)
+  sentence = "Hello there, and how are you?"
+  sentence.collect do |sentence|
+ #{sentence}
 end
 end
 
